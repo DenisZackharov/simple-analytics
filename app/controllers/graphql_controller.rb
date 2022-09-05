a229fe1ea3be25243802e566cb2fc9f8aeace348
deniszackharov <denis.zaharov@flatstack.com>
2022-09-05 18:16:24 +0300

    set up graphql

---
+++ app/controllers/graphql_controller.rb
@@ -0,0 +1,56 @@
+class GraphqlController < ApplicationController
+  def execute
+    render json: execute_query
+  rescue StandardError => e
+    raise e unless Rails.env.development?
+
+    handle_error_in_development e
+  end
+
+  private
+
+  def execute_query
+    ApplicationSchema.execute(
+      params[:query],
+      variables: ensure_hash(params[:variables]),
+      context: execution_context,
+      operation_name: params[:operationName]
+    )
+  end
+
+  def execution_context
+    current_user_context.merge(extensions: ensure_hash(params[:extensions]))
+  end
+
+  def current_user_context
+    return {} unless respond_to?(:current_user)
+
+    {
+      current_user: current_user,
+      token: token,
+      token_payload: payload
+    }
+  end
+
+  def ensure_hash(ambiguous_param)
+    case ambiguous_param
+    when String
+      ambiguous_param.present? ? ensure_hash(JSON.parse(ambiguous_param)) : {}
+    when Hash, ActionController::Parameters
+      ambiguous_param
+    when nil
+      {}
+    else
+      raise ArgumentError, "Unexpected parameter: #{ambiguous_param}"
+    end
+  end
+
+  def handle_error_in_development(exception)
+    logger.error exception.message
+    logger.error exception.backtrace.join("\n")
+
+    error = { error: { message: exception.message, backtrace: exception.backtrace }, data: {} }
+
+    render json: error, status: :internal_server_error
+  end
+end
